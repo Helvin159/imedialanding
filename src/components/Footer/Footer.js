830ffcdf12b06be31f0cb56d683fd0f4a133ced0
Helvin159 <Helvin159@gmail.com>
2021-11-04 08:31:32 -0400

    updates

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -24,7 +24,7 @@ const Footer = () => {
 			</Container>
 			<Container className='footer__menu' fluid>
 				<Row>
-					<Col md={1}>
+					<Col>
 						<h3>PHOTOGRAPHY</h3>
 						<ul>
 							<li>Leica M</li>
@@ -39,37 +39,61 @@ const Footer = () => {
 							<li>Leica M</li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>SPORT OPTICS</h3>
 						<ul>
 							<li>COMPACT</li>
+							<li>COMPACT</li>
+							<li>COMPACT</li>
+							<li>COMPACT</li>
+							<li>COMPACT</li>
+							<li>COMPACT</li>
+							<li>COMPACT</li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>BRAND ACCESSORIES</h3>
 						<ul>
 							<li>BAGS &amp; CASES </li>
+							<li>BAGS &amp; CASES </li>
+							<li>BAGS &amp; CASES </li>
+							<li>BAGS &amp; CASES </li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>PRE-OWNED</h3>
 						<ul>
 							<li>M SYSTEM</li>
+							<li>M SYSTEM</li>
+							<li>M SYSTEM</li>
+							<li>M SYSTEM</li>
+							<li>M SYSTEM</li>
+							<li>M SYSTEM</li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>WORLD OF LEICA</h3>
 						<ul>
 							<li>LEICA STORES</li>
+							<li>LEICA STORES</li>
+							<li>LEICA STORES</li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>LEICA AKADEMIE</h3>
 						<ul>
 							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
+							<li>DESTINATIONS</li>
 						</ul>
 					</Col>
-					<Col md={1}>
+					<Col>
 						<h3>LEICA CAMERA INC.</h3>
 					</Col>
 				</Row>
@@ -77,6 +101,20 @@ const Footer = () => {
 			<Container fluid className='footer__social'>
 				<Container className='footer__social__buttons'>
 					<h3>FOLLOW US</h3>
+					<ul>
+						<li>
+							<i class='fab fa-facebook-square'></i>
+						</li>
+						<li>
+							<i class='fab fa-instagram-square'></i>
+						</li>
+						<li>
+							<i class='fab fa-youtube'></i>
+						</li>
+						<li>
+							<i class='fab fa-twitter'></i>
+						</li>
+					</ul>
 				</Container>
 			</Container>
 			<Container fluid className='footer__copy'>
@@ -93,7 +131,9 @@ const Footer = () => {
 							<li>Sitemap</li>
 						</ul>
 					</Col>
-					<Col md={2}></Col>
+					<Col md={2}>
+						<Button>FIND A LEICA STORE</Button>
+					</Col>
 				</Row>
 			</Container>
 		</Container>
